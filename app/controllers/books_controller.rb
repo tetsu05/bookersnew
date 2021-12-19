20cafcd2fd91303b44ede3ae22f5b1047ee9482f
tetsu05 <tetsu051955@gmail.com>
2021-12-19 15:39:33 +0000

    Bookers復習

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,26 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
   
   def show
   end
-  
+
   def edit
   end
-  
+
   def destroy
   end
-  
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
