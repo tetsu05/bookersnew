36a40e76d52ff94cfa83604e6c5ecff859129b00
tetsu05 <tetsu051955@gmail.com>
2021-12-22 14:29:25 +0000

    Bookers復習

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,19 @@ class BooksController < ApplicationController
     book.save
     redirect_to books_path
   end
-  
+
   def show
     @book = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path
   end
 
   def destroy
